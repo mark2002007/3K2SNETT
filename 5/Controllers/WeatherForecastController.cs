94daf65a017ebf72694346b20fa58bdff414cbdf
Маркіян Мандзак <markmandzak2002@gmail.com>
2022-05-06 15:23:12 +0300

    Task 5.1

---
+++ 5/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace _5.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ICustomLogger _logger;
+
+    public WeatherForecastController(ICustomLogger logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        _logger.Log($"{DateTime.Now} : Get : WeatherForecast");
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
--- 5/Program.cs
+++ 5/Program.cs
@@ -1,19 +1,28 @@
-﻿using System;
-using static System.Console;
+using _5;
 
-namespace _5
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICustomLogger, CustomFileLogger>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CustomWeatherApi api = new CustomWeatherApi(new CustomFileLogger());
-            api.GetData(); 
-            Thread.Sleep(3000);
-            api.GetData(); 
-            Thread.Sleep(2000);
-            api.GetData(); 
-            Thread.Sleep(1000);
-        }   
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
